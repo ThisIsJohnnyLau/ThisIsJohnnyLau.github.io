2d2256b682948910f9ca9f1e7da31e397c6b4f7f
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-10-11 00:49:00 +0100

    More pics

---
+++ docs/update_scripts/presentations.R
@@ -0,0 +1,4 @@
+library(knitr)
+library(here)
+
+file.copy(here("../../deloitte_productivity_project/presentation.pptx"), here("docs/uk_gdp.pptx"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)

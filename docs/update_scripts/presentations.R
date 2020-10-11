ff4c64d4b20c43f61f6583f4176febf66d80cefe
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-10-12 00:36:26 +0100

    added bond analysis

--- docs/update_scripts/presentations.R
+++ docs/update_scripts/presentations.R
@@ -2,3 +2,5 @@ library(knitr)
 library(here)
 
 file.copy(here("../../deloitte_productivity_project/presentation.pptx"), here("docs/uk_gdp.pptx"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+
+file.copy(here("../../ethics_presentation/powerpoint/code_of_conduct_presentation.pptx"), here("docs/ethics.pptx"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)

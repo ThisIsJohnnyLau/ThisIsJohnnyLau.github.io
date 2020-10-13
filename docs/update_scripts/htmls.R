7568e263558f44e296be86007c050193d2c5a897
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-10-13 22:01:56 +0100

    added equity analysis examples

--- docs/update_scripts/htmls.R
+++ docs/update_scripts/htmls.R
@@ -9,3 +9,8 @@ file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis
 file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis_and_documentation/analysis.html"), here("docs/task_6.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
 
 
+# Investment analysis
+
+file.copy(here("../../../../investing/investment_analysis/security_analysis/bond_analysis/bond_analysis.html"), here("docs/bond_analysis.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+
+file.copy(here("../../../../investing/investment_analysis/security_analysis/equity_analysis_in_R/equity_valuation.html"), here("docs/equity_valuation.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)

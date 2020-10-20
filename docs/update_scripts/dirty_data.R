81e140d362beace87c15ada946b6b6863af5822d
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-10-20 19:23:19 +0100

    fixed dirty data links

--- docs/update_scripts/dirty_data.R
+++ docs/update_scripts/dirty_data.R
@@ -2,10 +2,10 @@ library(knitr)
 library(here)
 
 file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis_and_documentation/analysis.html"), here("docs/task_1.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
-file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis_and_documentation/analysis.html"), here("docs/task_2.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
-file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis_and_documentation/analysis.html"), here("docs/task_3.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
-file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis_and_documentation/analysis.html"), here("docs/task_4.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
-file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis_and_documentation/analysis.html"), here("docs/task_5.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
-file.copy(here("../../dirty_data_project_files/dirty_data_project/task1/analysis_and_documentation/analysis.html"), here("docs/task_6.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+file.copy(here("../../dirty_data_project_files/dirty_data_project/task2/analysis_and_documentation/analysis.html"), here("docs/task_2.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+file.copy(here("../../dirty_data_project_files/dirty_data_project/task3/analysis_and_documentation/analysis.html"), here("docs/task_3.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+file.copy(here("../../dirty_data_project_files/dirty_data_project/task4/analysis_and_documentation/analysis.html"), here("docs/task_4.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+file.copy(here("../../dirty_data_project_files/dirty_data_project/task5/analysis_and_documentation/analysis.html"), here("docs/task_5.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+file.copy(here("../../dirty_data_project_files/dirty_data_project/task6/analysis_and_documentation/analysis.html"), here("docs/task_6.html"), overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
 
 
